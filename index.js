5a02875d6dce76f350bb1fe9df2fcf99849c9551
Nathan Thomas <nthoma23@its.jnj.com>
2018-10-19 20:58:46 -0400

    Add basic comprehend sdk integration
    - POST with body parameter text and get comprehend data back

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-exports.handler = async (event) => {
-    const response = {
-        statusCode: 200,
-        body: event.body
+const AWS = require('aws-sdk');
+// import individual service
+// import comprehendClient = require('aws-sdk/clients/comprehend');
+
+exports.handler = (event, context, callback) => {
+
+    var message = JSON.parse(event.body);
+    console.log(message);
+    var comprehend = new AWS.Comprehend({apiVersion: '2017-11-27'});
+    var params = {
+        Text: message.text,
+        LanguageCode: 'en'
     };
-    return response;
+
+    comprehend.detectSentiment(params, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else {
+        console.log(data);
+
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(data)
+        };
+        callback(null, response);
+    }
+    });
+
+    // const response = {
+    //     statusCode: 200,
+    //     body: event.body
+    // };
+    // return response;
 };
