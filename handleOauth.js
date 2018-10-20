1dd6cf04d5a311ce49b86468d82a66e1a466b449
Nathan Thomas <nthoma23@its.jnj.com>
2018-10-20 17:48:24 -0400

    Save oauth token into dynamo db

--- handleOauth.js
+++ handleOauth.js
@@ -1,22 +1,13 @@
-const evaluate = require('evaluate');
-const querystring = require('querystring');
+const registerOauth = require('registerOauth');
 
 exports.handler = (event, context, callback) => {
-    // Currently stubbed to just echo
-    console.log(event.body);
-    // const message = querystring.parse(event.body)
-    // evaluate(message.text, (result) => {
-    //     const response = {
-    //         statusCode: 200,
-    //         body: event.body
-    //     }
-
-    //     callback(null, response);
-    // });
-
-    const response = {
-        statusCode: 200,
-        body: event.body
-    }
-    callback(null, response);
+    console.log(event.queryStringParameters);
+    registerOauth(event.queryStringParameters.code, (result) => {
+        const response = {
+            statusCode: 200,
+            headers: {"content-type": "text/html"},
+            body: '<p>Thank you for registering with our Application!<p>'
+        }
+        callback(null, response);
+    });
 };
