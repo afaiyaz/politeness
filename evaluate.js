a422acf5111eee624d49062f4b3fd9ef94817866
Nathan Thomas <nthoma23@its.jnj.com>
2018-10-20 19:24:00 -0400

    Added check for oauth token, and send registration url if they need it

--- evaluate.js
+++ evaluate.js
@@ -1,15 +1,64 @@
 const AWS = require('aws-sdk');
 
+
 const evaluate = (message, callback) => {
-    fetchResults(message).then((result) => {
+    getUserOauth(message)
+    .then((oauthResult) => {
+        console.log("*** Oauth Result ***", oauthResult);
+
+        const oauth = ((oauthResult.Item || {}).access_token || {}).S;
+        if (!oauth) {
+            let client_id = process.env.CLIENT_ID;
+            let team = message.team_id;
+            let response = {
+                text: `It looks like you haven't agreed to use this app, please click here to continue: https://slack.com/oauth/authorize?client_id=${client_id}&scope=chat:write:user&team=${team}`
+            }
+
+            console.log('Rejecting user for no oauth token', response);
+            callback(response);
+            throw new Error('No oauth token for user');
+        }
+
+        return {
+            ...message,
+            oauth
+        };
+    })
+    .then(fetchResults)
+    .then((result) => {
+        console.log(result);
         callback(evaluateResults(message, result));
     });
 };
 
+const getUserOauth = async (message) => {
+    var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+    var params = {
+        TableName: 'politeness-oauth',
+        Key: {
+            user_id: {
+                S: message.user_id
+            }
+        },
+        ProjectionExpression: "access_token",
+        ConsistentRead: true,
+    };
+
+    return new Promise ((resolve, reject) => {
+        dynamodb.getItem(params, function(err, data) {
+            if (err) reject(err);
+            else resolve(data);
+        });
+    });
+}
+
 const fetchResults = async (message) => {
+    console.log('*** fetchResults ***', message);
+
     var comprehend = new AWS.Comprehend({apiVersion: '2017-11-27'});
     var params = {
-        Text: message,
+        Text: message.text,
         LanguageCode: 'en'
     };
 
--- handlePoliteness.js
+++ handlePoliteness.js
@@ -4,7 +4,7 @@ const querystring = require('querystring');
 exports.handler = (event, context, callback) => {
     console.log(event.body);
     const message = querystring.parse(event.body)
-    evaluate(message.text, (result) => {
+    evaluate(message, (result) => {
         const response = {
             statusCode: 200,
             body: JSON.stringify(result)
